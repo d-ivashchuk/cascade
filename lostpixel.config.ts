e99196db4eeff3e93917ba31b2a3d7fd14879dd7
d-ivashchuk <dimaiv@me.com>
2024-03-24 19:22:39 +0100

    add breakpoints

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,6 +3,7 @@ import { CustomProjectConfig } from "lost-pixel";
 export const config: CustomProjectConfig = {
   storybookShots: {
     storybookUrl: "./storybook-static",
+    breakpoints: [320, 1024],
   },
 
   lostPixelProjectId: "clu5tpynq57cle90eeq0uyb8y",
