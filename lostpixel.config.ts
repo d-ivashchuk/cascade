13424ca30ceba228adeed25f6e11f3e3569f04b9
d-ivashchuk <dimaiv@me.com>
2024-03-24 19:03:58 +0100

    add lostpixel

---
+++ lostpixel.config.ts
@@ -0,0 +1,10 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: "./storybook-static",
+  },
+
+  lostPixelProjectId: "clu5tpynq57cle90eeq0uyb8y",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
