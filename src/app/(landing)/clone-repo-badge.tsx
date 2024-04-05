fddbbcf7068f0212d64f44296f75ba8ba391b4c3
d-ivashchuk <dimaiv@me.com>
2024-04-05 15:15:16 +0200

    hide clone badge on smaller screens

--- src/app/(landing)/clone-repo-badge.tsx
+++ src/app/(landing)/clone-repo-badge.tsx
@@ -9,18 +9,20 @@ const CloneRepoBadge = () => {
   const [_, copy] = useCopyToClipboard();
   const cloneText = "git clone https://github.com/d-ivashchuk/cascade.git";
   return (
-    <Badge
-      className="mx-auto mt-4 cursor-pointer px-4 py-2"
-      style={{
-        width: "fit-content",
-      }}
-      onClick={async () => {
-        await copy(cloneText);
-        toast.success("Copied to clipboard");
-      }}
-    >
-      <pre>{cloneText}</pre>
-    </Badge>
+    <div className="hidden md:block">
+      <Badge
+        className="mx-auto mt-4 cursor-pointer px-4 py-2"
+        style={{
+          width: "fit-content",
+        }}
+        onClick={async () => {
+          await copy(cloneText);
+          toast.success("Copied to clipboard");
+        }}
+      >
+        <pre>{cloneText}</pre>
+      </Badge>
+    </div>
   );
 };
 
