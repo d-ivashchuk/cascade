16dd7d0b4842450d37b842cc6654dea03366e31f
d-ivashchuk <dimaiv@me.com>
2024-04-08 09:53:35 +0200

    modify sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,9 @@
+import { allPosts } from "contentlayer/generated";
 import { type MetadataRoute } from "next";
 import { env } from "~/env.mjs";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const posts = allPosts;
   return [
     {
       url: env.NEXT_PUBLIC_DEPLOYMENT_URL,
@@ -9,6 +11,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
-    //extend sitemap with other static or dynamic routes
+    ...posts.map((post) => ({
+      url: `${env.NEXT_PUBLIC_DEPLOYMENT_URL}/${post.slug}`,
+      lastModified: new Date(post.date),
+      priority: 0.7,
+    })),
   ];
 }
